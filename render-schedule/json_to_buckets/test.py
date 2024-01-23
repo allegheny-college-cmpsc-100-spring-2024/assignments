90f376e3fcf2f79a8e5019a827ca2c34b6ab7203
kaibas01 <kaibas01@allegheny.edu>
2024-01-23 09:54:54 -0500

    schedule file

---
+++ render-schedule/json_to_buckets/test.py
@@ -0,0 +1,17 @@
+"""This is a test file for testing the new codes added before adding it to the main file """
+
+def main():
+    filename = "src/json_to_buckets/schedule.JSON"
+    with open(filename, "w") as fh:
+        content = fh.read()
+        print(content)
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
