25e4a95ceb2308e7a9f0e41cd73dd87c7c268e90
kaibas01 <kaibas01@allegheny.edu>
2024-01-23 09:57:53 -0500

    json file opens in code

--- render-schedule/json_to_buckets/test.py
+++ render-schedule/json_to_buckets/test.py
@@ -1,10 +1,13 @@
 """This is a test file for testing the new codes added before adding it to the main file """
 
+import json
+
 def main():
-    filename = "src/json_to_buckets/schedule.JSON"
-    with open(filename, "w") as fh:
-        content = fh.read()
-        print(content)
+    jsonFile = open("schedule.json")
+    data = json.load(jsonFile)
+
+    for line in data:
+        print(line)
     
 
 
