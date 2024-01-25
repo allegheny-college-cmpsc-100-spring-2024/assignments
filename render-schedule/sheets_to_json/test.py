9485dbccb9d0e1a5ce15edc97c70616f72de8088
kaibas01 <kaibas01@allegheny.edu>
2024-01-25 13:24:19 -0500

    code

--- render-schedule/sheets_to_json/test.py
+++ render-schedule/sheets_to_json/test.py
@@ -1,15 +1,18 @@
-"""This is a test file for testing the new codes added before adding it to the main file """
 
+#C:\Users\kaiba\OneDrive\Asztali gép\assignments\render-schedule\sheets_to_json\test.py
 import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 def main():
-    jsonFile = open("schedule.json")
-    data = json.load(jsonFile)
-
-    for key, value in data['due:'].items():
-        print(key, value)
-
-
+    #https://docs.google.com/spreadsheets/d/104OkqDtqq2ghhPYb01ct3KtmoegtOU3iLnL3DVIXOvo/edit#gid=0
+    #https://spreadsheets.google.com/feeds
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('test.json', scope)
+    gc = gspread.authorize(credentials)
+    mySheet = gc.open('CMPSC-100 Assignment Schedule')
+    worksheet = mySheet.worksheet('Sheet1')
+    print(worksheet)
 
 
 
